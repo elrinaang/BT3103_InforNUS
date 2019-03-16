3e828dce0c5825e3cfdd93d34d79a3babc0d62ca
elrinaang <39240333+elrinaang@users.noreply.github.com>
2019-03-17 01:09:58 +0800

    Lodging
    Changed the lodging page to add the filter function and added new pictures

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,24 @@
 <template>
   <div id="app">
-    <HomePage /> hi
+    <NavBar /> <halls />
 
-    <router-link :to="{ name: 'home' }">Hello</router-link>
-    <router-view></router-view>
+    <!--
+      <router-link :to="{ name: 'home' }">Hello</router-link>
+      <router-view></router-view>
+    -->
   </div>
 </template>
 
 <script>
 import NavBar from "./components/NavBar.vue";
 import HomePage from "./components/HomePage.vue";
-
+import halls from "./components/halls.vue";
 export default {
   name: "App",
   components: {
     NavBar,
-    HomePage
+    HomePage,
+    halls
   }
 };
 </script>
